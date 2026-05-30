c59952af4ca6d74fa15d9eb7d760685eb0617a85
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-05-31 00:14:30 +0200

    Update photos.js

--- functions/api/photos.js
+++ functions/api/photos.js
@@ -4,6 +4,7 @@ export async function onRequest(context) {
   const photos = list.objects.map(obj => ({
     id: obj.key,
     name: obj.key,
+    // správná cesta je přímo root bucketu
     url: `https://pub-04881c4bbea24b2ab23b9be5a7bd0aa1.r2.dev/${obj.key}`
   }));
   return new Response(JSON.stringify(photos), {
