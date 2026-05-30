369adab25426509df36b507902fe7b886f796be7
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-05-30 23:24:53 +0200

    Update upload.js

--- functions/api/upload.js
+++ functions/api/upload.js
@@ -5,14 +5,20 @@ export async function onRequestPost(context) {
   if (!file || typeof file === "string") {
     return new Response("Missing file", { status: 400 });
   }
+
   const fileName = file.name || `photo-${Date.now()}`;
   await env.PHOTOS.put(fileName, file.stream(), {
     httpMetadata: { contentType: file.type }
   });
 
+  // Přímý URL k obrázku
   const url = `https://pub-9ba0c4a1d5fc4ddabafac51f4f45d139.r2.dev/${fileName}`;
 
-  return new Response(JSON.stringify({ success: true, fileName, url }), {
+  return new Response(JSON.stringify({
+    success: true,
+    fileName,
+    url
+  }), {
     headers: { "Content-Type": "application/json" }
   });
 }
