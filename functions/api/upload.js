f480b74664d286e4a787e38e302f7d04d50001a4
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-05-31 21:10:58 +0200

    Update upload.js

--- functions/api/upload.js
+++ functions/api/upload.js
@@ -1,19 +1,14 @@
-/ functions/api/upload.js
 export async function onRequestPost(context) {
   const { R2 } = context.env;
-
   const form = await context.request.formData();
   const file = form.get("file");
 
-  if (!file) {
-    return new Response("Missing file", { status: 400 });
-  }
+  if (!file) return new Response("Missing file", { status: 400 });
 
   const buffer = await file.arrayBuffer();
 
   await R2.put(file.name, buffer, {
     httpMetadata: { contentType: file.type }
-    // customMetadata: { exif: "...", tags: "..." }  // připravené místo
   });
 
   return new Response(JSON.stringify({ success: true }), {
