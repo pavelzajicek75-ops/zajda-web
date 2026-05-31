3b521389f56baaa4f04c3496ee65f69c9675aa40
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-05-31 16:28:44 +0200

    Create photos.js

---
+++ functions/api/photos.js
@@ -0,0 +1,15 @@
+export async function onRequest() {
+  const photos = [
+    "IMG_7781.JPG",
+    "IMG_7782.JPG",
+    "IMG_7783.JPG"
+  ];
+
+  const urls = photos.map(name => ({
+    url: `https://pub-04881c4bbea24b2ab23b9be5a7bd0aa1.r2.dev/${name}`
+  }));
+
+  return new Response(JSON.stringify(urls), {
+    headers: { "Content-Type": "application/json" }
+  });
+}
