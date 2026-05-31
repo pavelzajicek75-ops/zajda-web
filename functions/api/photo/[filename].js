1536e790944132cc794d91cf30b9f029e821abee
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-05-31 16:38:24 +0200

    Update [filename].js

--- functions/api/photo/[filename].js
+++ functions/api/photo/[filename].js
@@ -2,7 +2,6 @@ export async function onRequest(context) {
   const { params } = context;
   const filename = params.filename;
 
-  // Public R2 URL – žádné bindingy
   const url = `https://pub-04881c4bbea24b2ab23b9be5a7bd0aa1.r2.dev/${filename}`;
 
   const response = await fetch(url);
