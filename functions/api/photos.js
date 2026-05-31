858756392c2e9dfff5705aa00b524478fbd05fa2
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-05-31 20:57:42 +0200

    Update photos.js

--- functions/api/photos.js
+++ functions/api/photos.js
@@ -1,17 +1,46 @@
+/ functions/api/photos.js
 export async function onRequest(context) {
   const { R2 } = context.env;
 
   const list = await R2.list();
 
-  const photos = list.objects.map(obj => ({
-    name: obj.key,
-    size: obj.size,
-    url: `https://pub-04881c4bbea24b2ab23b9be5a7bd0aa1.r2.dev/${obj.key}`
-  }));
+  const photos = [];
+
+  for (const obj of list.objects) {
+    let exif = {};
+    let tags = [];
+
+    if (obj.customMetadata) {
+      if (obj.customMetadata.exif) {
+        try {
+          exif = JSON.parse(obj.customMetadata.exif);
+        } catch (e) {
+          exif = {};
+        }
+      }
+      if (obj.customMetadata.tags) {
+        try {
+          tags = JSON.parse(obj.customMetadata.tags);
+        } catch (e) {
+          tags = [];
+        }
+      }
+    }
+
+    photos.push({
+      name: obj.key,
+      size: obj.size,
+      url: `https://pub-04881c4bbea24b2ab23b9be5a7bd0aa1.r2.dev/${obj.key}`,
+      exif,
+      tags
+    });
+  }
+
+  const totalSize = list.objects.reduce((sum, o) => sum + o.size, 0);
 
   return new Response(JSON.stringify({
     totalCount: photos.length,
-    totalSize: list.objects.reduce((sum, o) => sum + o.size, 0),
+    totalSize,
     photos
   }), {
     headers: { "Content-Type": "application/json" }
