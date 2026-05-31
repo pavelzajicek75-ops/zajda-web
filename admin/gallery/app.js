bfe105f22e08c2331d41301346b05cbbe0228421
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-05-31 11:23:03 +0200

    Update app.js

--- admin/gallery/app.js
+++ admin/gallery/app.js
@@ -5,7 +5,6 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   photos.forEach(photo => {
     const img = document.createElement("img");
-    // Použij skutečný public endpoint pro svůj bucket
     img.src = `https://pub-04881c4bbea24b2ab23b9be5a7bd0aa1.r2.dev/${photo.name}`;
     img.alt = photo.name;
     gallery.appendChild(img);
