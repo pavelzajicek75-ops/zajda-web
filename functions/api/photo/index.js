fafa95de977ecf352d20f1f8497de50518b8013f
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-06-02 22:18:05 +0200

    Create index.js

---
+++ functions/api/photo/index.js
@@ -0,0 +1,14 @@
+export async function onRequest(context) {
+    const bucket = context.env.zajda_photos;
+
+    const list = await bucket.list();
+
+    const items = list.objects.map(obj => ({
+        filename: obj.key,
+        url: `https://pub-9ba0c4a1d5fc4ddabafac51f4f45d139.r2.dev/${obj.key}`
+    }));
+
+    return new Response(JSON.stringify(items), {
+        headers: { "Content-Type": "application/json" }
+    });
+}
