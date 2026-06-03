953b671f2c414a08cf017bae997fafed32155132
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-06-03 15:35:41 +0200

    Update index.js

--- functions/api/photo/index.js
+++ functions/api/photo/index.js
@@ -7,7 +7,7 @@ export async function onRequest(context) {
   const items = list.objects.map(obj => ({
     filename: obj.key,
     size: obj.size,
-    url: `https://pub-9ba0c4a1d5fc4ddabafac51f4f45d139.r2.dev/${encodeURIComponent(obj.key)}`
+    url: `/api/photo/${encodeURIComponent(obj.key)}`
   }));
 
   return new Response(JSON.stringify(items), {
