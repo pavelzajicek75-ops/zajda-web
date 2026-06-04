46a5834b9bceda8b7115ec9d28be6528a0f1b1d1
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-06-04 15:51:35 +0200

    Update save.js

--- functions/api/articles/sections/save.js
+++ functions/api/articles/sections/save.js
@@ -1,15 +1,5 @@
 // /functions/api/articles/sections/save.js
-export async function onRequestPost(context) {
-  const { request, env } = context;
-  const bucket = env.zajda_articles;
-
-  const body = await request.json();
-  const sections = body.sections;
-
-  await bucket.put("sections.json", JSON.stringify(sections, null, 2), {
-    httpMetadata: { contentType: "application/json" }
-  });
-
+export async function onRequestPost() {
   return new Response(JSON.stringify({ success: true }), {
     status: 200,
     headers: { "Content-Type": "application/json" }
