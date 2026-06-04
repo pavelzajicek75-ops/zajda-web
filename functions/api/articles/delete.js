f914778525b422d00bff4b39049867990cd1aa35
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-06-04 10:39:33 +0200

    Update delete.js

--- functions/api/articles/delete.js
+++ functions/api/articles/delete.js
@@ -1,1 +1,9 @@
+export async function onRequestPost(context) {
+  const body = await context.request.json();
+  const key = body.key;
+
+  await context.env.ARTICLES_BUCKET.delete(key);
+
+  return new Response("OK");
+}
 
