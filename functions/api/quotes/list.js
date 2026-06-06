ff9fc77f8550a8cfa0d32b4c1c2d2a2d0535e1ea
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-06-06 17:54:48 +0200

    Update list.js

--- functions/api/quotes/list.js
+++ functions/api/quotes/list.js
@@ -1,16 +1,15 @@
 export async function onRequest(context) {
   const { env } = context;
 
-  const list = await env.QUOTES.list();
-  const quotes = [];
-
-  for (const key of list.keys) {
-    const raw = await env.QUOTES.get(key.name);
-    if (!raw) continue;
-
-    const data = JSON.parse(raw);
-    quotes.push(data);
+  const object = await env.QUOTES_R2.get("quotes.json");
+  if (!object) {
+    return new Response(JSON.stringify({ quotes: [] }), {
+      headers: { "Content-Type": "application/json" }
+    });
   }
+
+  const data = await object.text();
+  const quotes = JSON.parse(data);
 
   return new Response(JSON.stringify({ quotes }), {
     headers: { "Content-Type": "application/json" }
