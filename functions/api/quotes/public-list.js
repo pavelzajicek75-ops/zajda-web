05c3c0e22349b2f908475ec3c252e955868f8663
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-06-06 17:57:51 +0200

    Update public-list.js

--- functions/api/quotes/public-list.js
+++ functions/api/quotes/public-list.js
@@ -1,18 +1,20 @@
 export async function onRequest(context) {
   const { env } = context;
 
-  const list = await env.QUOTES.list();
-  const quotes = [];
-
-  for (const key of list.keys) {
-    const raw = await env.QUOTES.get(key.name);
-    if (!raw) continue;
-
-    const data = JSON.parse(raw);
-    quotes.push({ text: data.text, author: data.author });
+  const object = await env.QUOTES_R2.get("quotes.json");
+  if (!object) {
+    return new Response(JSON.stringify({ quotes: [] }), {
+      headers: { "Content-Type": "application/json" }
+    });
   }
 
-  return new Response(JSON.stringify({ quotes }), {
+  const quotes = JSON.parse(await object.text());
+  const publicQuotes = quotes.map(q => ({
+    text: q.text,
+    author: q.author
+  }));
+
+  return new Response(JSON.stringify({ quotes: publicQuotes }), {
     headers: { "Content-Type": "application/json" }
   });
 }
