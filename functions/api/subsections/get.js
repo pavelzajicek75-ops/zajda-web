eb85c44216b621dafe7abe608bb49266f612db49
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-06-06 22:11:59 +0200

    Update get.js

--- functions/api/subsections/get.js
+++ functions/api/subsections/get.js
@@ -1,1 +1,8 @@
+export async function onRequest(context) {
+  const bucket = context.env.SECTIONS_BUCKET;
+  const file = await bucket.get("sections/subsections.json");
+  if (!file) return Response.json({});
+  const json = await file.json();
+  return Response.json(json);
+}
 
