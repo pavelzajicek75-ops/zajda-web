cdfdf1c0e4c82426e05f74a317a31d5844df6539
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-06-06 22:13:20 +0200

    Create create.js

---
+++ functions/api/articles/create.js
@@ -0,0 +1,12 @@
+export async function onRequest(context) {
+  const bucket = context.env.ARTICLES_BUCKET;
+
+  const body = await context.request.json();
+  const id = body.id;
+
+  await bucket.put(`articles/${id}.json`, JSON.stringify(body), {
+    httpMetadata: { contentType: "application/json" }
+  });
+
+  return Response.json({ ok: true });
+}
