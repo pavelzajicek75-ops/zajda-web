2f35088caba11d7729e02bd5fa0d33feac538a53
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-06-06 17:57:09 +0200

    Update save.js

--- functions/api/quotes/save.js
+++ functions/api/quotes/save.js
@@ -3,14 +3,17 @@ export async function onRequest(context) {
   const body = await request.json();
 
   const object = await env.QUOTES_R2.get("quotes.json");
-  const data = object ? JSON.parse(await object.text()) : [];
+  const quotes = object ? JSON.parse(await object.text()) : [];
 
-  const existing = data.find(q => q.id === body.id);
-  if (existing) {
-    existing.text = body.text;
-    existing.author = body.author;
+  if (body.id) {
+    const q = quotes.find(x => x.id === body.id);
+    if (q) {
+      q.text = body.text;
+      q.author = body.author;
+      q.updated = Date.now();
+    }
   } else {
-    data.push({
+    quotes.push({
       id: crypto.randomUUID(),
       text: body.text,
       author: body.author,
@@ -18,7 +21,7 @@ export async function onRequest(context) {
     });
   }
 
-  await env.QUOTES_R2.put("quotes.json", JSON.stringify(data));
+  await env.QUOTES_R2.put("quotes.json", JSON.stringify(quotes));
 
   return new Response(JSON.stringify({ ok: true }), {
     headers: { "Content-Type": "application/json" }
