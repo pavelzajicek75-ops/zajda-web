7ea7325570bd2794c5dde86795f4877235986dee
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-06-07 20:01:34 +0200

    Create dashboard-animations.js

---
+++ public/js/dashboard-animations.js
@@ -0,0 +1,5 @@
+// Animace hlavního nadpisu
+document.addEventListener("DOMContentLoaded", () => {
+  const title = document.querySelector(".animated-title span");
+  title.style.animation = "reveal 3s ease forwards";
+});
