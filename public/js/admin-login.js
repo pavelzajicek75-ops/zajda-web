efe33e5918091ad262e752ec66f76a52c543d4da
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-06-07 20:03:14 +0200

    Update admin-login.js

--- public/js/admin-login.js
+++ public/js/admin-login.js
@@ -1,8 +1,8 @@
 document.getElementById("loginBtn").onclick = () => {
   const pwd = document.getElementById("password").value;
 
-  if (pwd === "Zajda2025") {   // 🔥 tvoje heslo
-    localStorage.setItem("adminAuth", "OK");
+  if (pwd === "Zajda2025") {
+    localStorage.setItem("adminAuth", "1");
     window.location.href = "/admin/dashboard.html";
   } else {
     alert("Špatné heslo!");
