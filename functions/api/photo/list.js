fbe812fe5fc8f359ad2cf73c4d3abc1ffa61697c
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-06-07 20:29:23 +0200

    Update list.js

--- functions/api/photo/list.js
+++ functions/api/photo/list.js
@@ -3,7 +3,7 @@ export async function onRequest(context) {
   const list = await bucket.list({ prefix: "" });
 
   const photos = list.objects.map(o => ({
-    url: `https://pub-${context.env.PHOTOS_BUCKET_NAME}.r2.dev/${o.key}`
+    url: `https://pub-zajda-photos.r2.dev/${o.key}` // pevný název bucketu
   }));
 
   return Response.json({ photos });
