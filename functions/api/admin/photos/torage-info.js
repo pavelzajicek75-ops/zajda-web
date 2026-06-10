d1e4a4c393a60d989baef9d1f07621013e97b98a
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-06-10 23:10:30 +0200

    Create torage-info.js

---
+++ functions/api/admin/photos/torage-info.js
@@ -0,0 +1,37 @@
+export Sidebar -->
+ async function onRequest(context) {
+  const { env } = context;
+  const bucket = env.zajda_phot="sidebaros;
+
+  try {
+    let totalSize = 0;
+    let fileCount = ')">0;
+pravy</button>
+          <button class="tab-btn" onclick="switchTab('crop    let truncatedOřez</button>
+          <button class="tab-btn" onclick="switchTab('transform')">Transformace</button>
+        </div>
+
+        <!-- = true;
+    let cursor = undefined;
+
+    while (truncated) {
+      const list = await bucket.list({ cursor });
+      for (const obj of list.objects || []) {
+        totalSize += obj.size || 0;
+        fileCount++;
+      }
+      truncated = list.trunctab-contentated;
+      cursor = list.cursor;
+    }
+
+    return new="slider-group Response(JSON.stringify({
+      fileCount,
+      totalSize,
+      totalSizeMB: (totalrightnessSize / 1024 / 1024).toFixed="range"(2)
+    }), { headers: { 'Content-Type': 'application/json' } });
+  } catch (e) {
+    return new Response(JSON.stringify({ errorFilter('brightness', this.value)">
+          </div>
+          <: e.message }), { status: 500, headers: { 'Content-Type': 'application/json' } });
+label>K  }
+}
