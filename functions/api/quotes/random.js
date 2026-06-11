0395039a6d471231e60d67eb216a9120c44ad0d7
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-06-11 21:16:23 +0200

    Create random.js

---
+++ functions/api/quotes/random.js
@@ -0,0 +1,19 @@
+export async function onRequest(context) {
+  const { QUOTES_BUCKET } = context.env;
+
+  const objects = await QUOTES_BUCKET.list({ prefix: "quotes/" });
+  if (!objects.objects.length) {
+    return new Response(JSON.stringify({ error: "No quotes available" }), {
+      status: 404,
+      headers: { "Content-Type": "application/json" }
+    });
+  }
+
+  const randomObj = objects.objects[Math.floor(Math.random() * objects.objects.length)];
+  const file = await QUOTES_BUCKET.get(randomObj.key);
+  const json = await file.json();
+
+  return new Response(JSON.stringify(json), {
+    headers: { "Content-Type": "application/json" }
+  });
+}
