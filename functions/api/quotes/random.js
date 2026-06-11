429c98081e092df97e6c805f375193985a5d3532
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-06-11 21:32:24 +0200

    Update random.js

--- functions/api/quotes/random.js
+++ functions/api/quotes/random.js
@@ -5,7 +5,10 @@ export async function onRequest(context) {
   if (!objects.objects.length) {
     return new Response(JSON.stringify({ error: "No quotes available" }), {
       status: 404,
-      headers: { "Content-Type": "application/json" }
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-store"
+      }
     });
   }
 
@@ -14,6 +17,9 @@ export async function onRequest(context) {
   const json = await file.json();
 
   return new Response(JSON.stringify(json), {
-    headers: { "Content-Type": "application/json" }
+    headers: {
+      "Content-Type": "application/json",
+      "Cache-Control": "no-store"
+    }
   });
 }
