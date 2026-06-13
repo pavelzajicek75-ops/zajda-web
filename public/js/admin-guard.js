0f54d0d6d657b2c30ee9dfca70c3402db03e8b27
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-06-13 15:25:41 +0200

    Delete public directory

--- public/js/admin-guard.js
+++
@@ -1,3 +0,0 @@
-if (localStorage.getItem("adminAuth") !== "1") {
-  window.location.href = "/admin/login.html";
-}
--- public/js/dashboard-animations.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const title = document.querySelector(".animated-title span");
-  title.style.animation = "reveal 3s ease forwards";
-});
