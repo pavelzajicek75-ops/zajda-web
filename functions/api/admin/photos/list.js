484d303b641d49b5b412242dd9be3205ffe98060
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-06-13 16:55:44 +0200

    Update list.js

--- functions/api/admin/photos/list.js
+++ functions/api/admin/photos/list.js
@@ -1,37 +1,17 @@
-export async function onRequest(context) {
-  const { request, env } = context;
-  const bucket = env.zajda_photos;
+export async function onRequestGet(context) {
+  const { env } = context;
 
-  if (request.method !== 'GET') {
-    return new Response('Method not allowed', { status: 405 });
-  }
+  const objects = await env.PHOTOS_R2.list();
 
-  try {
-    const prefix = 'photos/meta/';
-    const listed = await bucket.list({ prefix: prefix });
-    const photos = [];
+  const photos = (objects.objects || []).map((obj) => ({
+    key: obj.key,
+    size: obj.size,
+    uploaded: obj.uploaded,
+    url: `${env.CDN_BASE_URL}/${obj.key}`,
+  }));
 
-    for (const obj of listed.objects || []) {
-      try {
-        const metaObj = await bucket.get(obj.key);
-        if (!metaObj) continue;
-        const metaText = await metaObj.text();
-        const meta = JSON.parse(metaText);
-        photos.push(meta);
-      } catch (e) {
-        continue;
-      }
-    }
-
-    photos.sort((a, b) => new Date(b.created || 0) - new Date(a.created || 0));
-
-    return new Response(JSON.stringify({ photos, count: photos.length }), {
-      headers: { 'Content-Type': 'application/json' }
-    });
-  } catch (e) {
-    return new Response(JSON.stringify({ error: e.message, photos: [] }), {
-      status: 500,
-      headers: { 'Content-Type': 'application/json' }
-    });
-  }
+  return new Response(JSON.stringify({ photos }), {
+    status: 200,
+    headers: { 'Content-Type': 'application/json' },
+  });
 }
