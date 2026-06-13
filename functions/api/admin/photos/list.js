15c6b1ce06d0410152d06354224ebd60901f48cb
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-06-13 17:27:24 +0200

    Update list.js

--- functions/api/admin/photos/list.js
+++ functions/api/admin/photos/list.js
@@ -1,17 +1,16 @@
 export async function onRequestGet(context) {
   const { env } = context;
-
+  
   const objects = await env.PHOTOS_R2.list();
-
-  const photos = (objects.objects || []).map((obj) => ({
+  
+  const photos = (objects.objects || []).map(obj => ({
     key: obj.key,
     size: obj.size,
     uploaded: obj.uploaded,
-    url: `${env.CDN_BASE_URL}/${obj.key}`,
+    url: `${env.CDN_BASE_URL}/${obj.key}`
   }));
 
   return new Response(JSON.stringify({ photos }), {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' },
+    headers: { "Content-Type": "application/json" }
   });
 }
