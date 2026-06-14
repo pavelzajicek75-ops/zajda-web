f08e3357101a1f3fc85eb03a3d0b071ec87fb82d
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-06-14 21:22:00 +0200

    Delete admin directory

--- admin/admin-guard.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  // dashboard používá sessionStorage + authToken
-  const token = sessionStorage.getItem("authToken");
-
-  if (!token) {
-    window.location.href = "/admin/login.html";
-    return;
-  }
-})();
