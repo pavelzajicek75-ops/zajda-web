b804716f0ef8d6fb3ae40d66f7b91533104da003
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-06-15 22:22:25 +0200

    Update delete.js

--- functions/api/quotes/delete.js
+++ functions/api/quotes/delete.js
@@ -1,8 +1,7 @@
-// functions/api/quotes/delete.js
 export async function onRequestDelete(context) {
   const { request, env } = context;
   const { searchParams } = new URL(request.url);
   const id = searchParams.get('id');
-  await env.QUOTES_R2.delete(`quotes/${id}.json`);
+  await env['zajda-quotes'].delete(`quotes/${id}.json`);
   return Response.json({ success: true });
 }
