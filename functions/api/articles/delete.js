a3e71d81b6ad8f6f07b15156803bdf8ef9532a77
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-06-15 17:10:32 +0200

    Update delete.js

--- functions/api/articles/delete.js
+++ functions/api/articles/delete.js
@@ -1,31 +1,8 @@
-// /functions/api/article/delete.js
+// functions/api/articles/delete.js
 export async function onRequestDelete(context) {
   const { request, env } = context;
-  const bucket = env.zajda_articles;
-
-  const url = new URL(request.url);
-  const id = url.searchParams.get("id");
-
-  if (!id) {
-    return new Response(JSON.stringify({ error: "Missing id" }), { status: 400 });
-  }
-
-  await bucket.delete(`articles/${id}.json`);
-
-  const listFile = await bucket.get("articles.json");
-  let list = [];
-
-  if (listFile) {
-    list = JSON.parse(await listFile.text());
-  }
-
-  list = list.filter(a => a.id !== id);
-
-  await bucket.put("articles.json", JSON.stringify(list, null, 2), {
-    httpMetadata: { contentType: "application/json" }
-  });
-
-  return new Response(JSON.stringify({ success: true }), {
-    headers: { "Content-Type": "application/json" }
-  });
+  const { searchParams } = new URL(request.url);
+  const id = searchParams.get('id');
+  await env.ARTICLES.delete(`article:${id}`);
+  return Response.json({ success: true });
 }
