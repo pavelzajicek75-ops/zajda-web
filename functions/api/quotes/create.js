ebf26db38200863f27387109d835efab26addff9
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-06-15 23:13:01 +0200

    Update create.js

--- functions/api/quotes/create.js
+++ functions/api/quotes/create.js
@@ -1,17 +1,13 @@
 export async function onRequestPost(context) {
   const { request, env } = context;
   const body = await request.json();
-  
   const id = crypto.randomUUID();
   const key = `quotes/${id}.json`;
-  
   const quote = {
     text: body.text || '',
     author: body.author || '',
     created: Date.now()
   };
-  
   await env['zajda-quotes'].put(key, JSON.stringify(quote));
-  
   return Response.json({ ...quote, key });
 }
