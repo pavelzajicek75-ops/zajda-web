cdaff3d0fac46738f3bdb43ecdb8e2f934a0a08e
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-06-15 22:21:46 +0200

    Update list.js

--- functions/api/quotes/list.js
+++ functions/api/quotes/list.js
@@ -1,11 +1,9 @@
-// functions/api/quotes/list.js
 export async function onRequestGet(context) {
   const { env } = context;
-  // DŮLEŽITÉ: Změň QUOTES_R2 na název tvého R2 bindingu
-  const list = await env.QUOTES_R2.list({ prefix: 'quotes/' });
+  const list = await env['zajda-quotes'].list({ prefix: 'quotes/' });
   const quotes = [];
   for (const obj of list.objects || []) {
-    const data = await env.QUOTES_R2.get(obj.key);
+    const data = await env['zajda-quotes'].get(obj.key);
     if (data) quotes.push(await data.json());
   }
   return Response.json(quotes);
