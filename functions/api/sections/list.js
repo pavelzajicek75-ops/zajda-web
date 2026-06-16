6491f1a66a6b68fbb7754fb003a05d407197702c
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-06-16 12:03:21 +0200

    Update list.js

--- functions/api/sections/list.js
+++ functions/api/sections/list.js
@@ -1,9 +1,9 @@
 export async function onRequestGet(context) {
   const { env } = context;
-  const list = await env.SECTIONS.list();
+  const list = await env.SUBSECTIONS.list({ prefix: 'section:' });
   const sections = [];
   for (const key of list.keys) {
-    const data = await env.SECTIONS.get(key.name, { type: 'json' });
+    const data = await env.SUBSECTIONS.get(key.name, { type: 'json' });
     if (data) sections.push(data);
   }
   return Response.json(sections.sort((a, b) => (a.order || 0) - (b.order || 0)));
