88e00f986b6ee834186de23c3e2c07f5af7a7eb8
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-06-16 09:39:58 +0200

    Create list.js

---
+++ functions/api/subsections/list.js
@@ -0,0 +1,15 @@
+export async function onRequestGet(context) {
+  const { env } = context;
+  const { searchParams } = new URL(request.url);
+  const sectionId = searchParams.get('sectionId');
+  
+  const list = await env.SUBSECTIONS.list();
+  const subsections = [];
+  for (const key of list.keys) {
+    const data = await env.SUBSECTIONS.get(key.name, { type: 'json' });
+    if (data && (!sectionId || data.sectionId === sectionId)) {
+      subsections.push(data);
+    }
+  }
+  return Response.json(subsections.sort((a, b) => (a.order || 0) - (b.order || 0)));
+}
