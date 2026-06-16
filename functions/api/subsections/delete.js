9a490f38b05f2b9345c8fc6095324566b56f3dcb
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-06-16 09:40:39 +0200

    Create delete.js

---
+++ functions/api/subsections/delete.js
@@ -0,0 +1,7 @@
+export async function onRequestDelete(context) {
+  const { request, env } = context;
+  const { searchParams } = new URL(request.url);
+  const id = searchParams.get('id');
+  await env.SUBSECTIONS.delete(`subsection:${id}`);
+  return Response.json({ success: true });
+}
