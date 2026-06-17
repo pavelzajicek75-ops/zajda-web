e92d5cbcb978a7f9f8b54bfb3827ce0c75a1e418
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-06-17 14:22:09 +0200

    Update get.js

--- functions/api/about/get.js
+++ functions/api/about/get.js
@@ -1,13 +1,5 @@
 export async function onRequestGet(context) {
   const { env } = context;
-  try {
-    const data = await env.ARTICLES.get('about:zajda', { type: 'json' });
-    if (data) return Response.json(data);
-  } catch {}
-  
-  return Response.json({
-    title: 'O Zajdovi',
-    text: 'Zatím žádný popis.',
-    photos: []
-  });
+  const data = await env.ARTICLES.get('about:zajda', { type: 'json' });
+  return Response.json(data || { title: 'O Zajdovi', text: '', photos: [], subsections: [] });
 }
