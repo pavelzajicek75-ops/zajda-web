3f9a9bb6b3e28b4322c0fe1ced8fc8287cc39081
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-06-18 21:50:43 +0200

    Update file.js

--- functions/api/photos/file.js
+++ functions/api/photos/file.js
@@ -1,16 +1,12 @@
 export async function onRequestGet(context) {
-  const { request, env } = context;
-  const { searchParams } = new URL(request.url);
+  const { env } = context;
+  const { searchParams } = new URL(context.request.url);
   const key = searchParams.get('key');
-
   if (!key) return new Response('Chybí key', { status: 400 });
-
   const object = await env.PHOTOS_R2.get(key);
   if (!object) return new Response('Nenalezeno', { status: 404 });
-
   const headers = new Headers();
   headers.set('Content-Type', object.httpMetadata?.contentType || 'image/jpeg');
   headers.set('Cache-Control', 'public, max-age=31536000');
-  
   return new Response(object.body, { headers });
 }
