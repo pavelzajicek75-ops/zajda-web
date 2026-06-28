edd6a3a08fb683da995b3f854b4e7ae65c16c18f
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-06-28 21:47:16 +0200

    Delete functions/article/[[id]].js

--- functions/article/[[id]].js
+++
@@ -1,8 +0,0 @@
-export async function onRequest(context) {
-  return context.env.ASSETS.fetch(
-    new Request(
-      new URL('/article.html', context.request.url),
-      context.request
-    )
-  );
-}
